1d4f9460319ae2d493eb99e4a66eb7f3fd8cea86
Jonas Hansen <jonas@jonas-hansen.dk>
2025-07-03 22:23:44 +0200

    Setup test script

--- boot.sh
+++ boot.sh
@@ -14,7 +14,10 @@ pacman -Q git &>/dev/null || sudo pacman -Sy --noconfirm --needed git
 
 echo -e "\nCloning Omarchy..."
 rm -rf ~/.local/share/omarchy/
-git clone https://github.com/basecamp/omarchy.git ~/.local/share/omarchy >/dev/null
+git clone https://github.com/jonashan/omarchy.git ~/.local/share/omarchy >/dev/null
+cd ~/.local/share/omarchy
+git fetch origin move-workspace-to-monitor && git checkout move-workspace-to-monitor
+cd -
 
 echo -e "\nInstallation starting..."
 source ~/.local/share/omarchy/install.sh
